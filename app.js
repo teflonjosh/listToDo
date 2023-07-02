ac8c150c1a46cdacf807903fe655c76678cb3d08
TeflonJosh <114349638+teflonjosh@users.noreply.github.com>
2023-07-02 11:14:52 -0400

    Update app.js
    Updates 7/2/2023: 
    1. Apply Load Button to load local storage;
    2. Update functions to Event Listeners and Not On Click's called in HTML;
    3. Streamline and adjust formatting of .js Code and Comments 
    //

--- app.js
+++ app.js
@@ -1,169 +1,189 @@
-   //######### TASKS ################################ 
-    // DOM ELEMENTS
-    const todoList = document.getElementById('todo-list');
-    const saveButton = document.getElementById('save-btn');
-    // DELETE TASKS DOM
-    const deleteAllButton = document.getElementById('delete-all-btn');
-    deleteAllButton.addEventListener('click', deleteAllTasks);
-    // EXPORT DOM
-    const exportButton = document.getElementById('export-btn');
-    exportButton.addEventListener('click', exportTodoList);
-       //PRINT
-       function exportTodoList() {
-        window.print();
-        }
-    // INPUT DOM
-    const todoInput = document.getElementById('input');
-    todoInput.addEventListener('keypress', function (event) {
-    if (event.key === 'Enter') {
-       addTask();
-        }
-        });
-      // FUNCTION TO ADD TASK
-        function addTask() {
-            const taskText = todoInput.value.trim();
-            if (taskText !== '') {
-                const taskElement = document.createElement('div');
-                taskElement.className = 'todo-item';
-                taskElement.innerHTML = `
-          <input type="checkbox" class="toggle-task">
-          <span>${taskText}</span>
-          <span class="delete-btn">X</span>
-        `;
-                const deleteButton = taskElement.querySelector('.delete-btn');
-                deleteButton.addEventListener('click', deleteTask);
-    
-                const toggleCheckbox = taskElement.querySelector('.toggle-task');
-                toggleCheckbox.addEventListener('change', toggleTask);
-    
-                todoList.appendChild(taskElement);
-                todoInput.value = '';
-        // SAVE ADDED TASK
-                saveTasks(); 
-            }
-        }
-        //DELETE A SINGLE TASK
-        function deleteTask(event) {
-            const taskElement = event.target.parentNode;
-            todoList.removeChild(taskElement);
-            saveTasks(); 
-        }
-        // DELETE ALL TASKS!!!!
-            function deleteAllTasks() {
-            todoList.innerHTML = '';
-            saveTasks(); 
-        }
-        //FUNCTION TO TOGGLE
-        function toggleTask(event) {
-            const taskElement = event.target.parentNode;
-            taskElement.classList.toggle('completed');
-            saveTasks();
-        }
-        //FUNCTION TO SAVE TASKS
-        function saveTasks() {
-            const tasks = [];
-            const taskElements = document.getElementsByClassName('todo-item');
-            for (let i = 0; i < taskElements.length; i++) {
-                const taskText = taskElements[i].querySelector('span').textContent;
-                const isCompleted = taskElements[i].classList.contains('completed');
-                tasks.push({ text: taskText, completed: isCompleted });
-            }
-            localStorage.setItem('tasks', JSON.stringify(tasks));
-            }
-
-        //LOAD LOCAL
-        function loadTasks() {
-            const tasks = localStorage.getItem('tasks');
-            if (tasks) {
-                const parsedTasks = JSON.parse(tasks);
-                parsedTasks.forEach((task) => {
-                    const taskElement = document.createElement('div');
-                    taskElement.className = 'todo-item';
-                    taskElement.innerHTML = `
-          <input type="checkbox" class="toggle-task">
-          <span>${task.text}</span>
-          <span class="delete-btn">X</span>
-        `;
-        // COMPLETED TASKS
-                if (task.completed) {
-                    taskElement.classList.add('completed');
-                }
-                const deleteButton = taskElement.querySelector('.delete-btn');
-                deleteButton.addEventListener('click', deleteTask);
-
-                const toggleCheckbox = taskElement.querySelector('.toggle-task');
-                toggleCheckbox.addEventListener('change', toggleTask);
-
-                todoList.appendChild(taskElement);
-            });
-          }
-        }
-    // LOAD SAVED TASKS
-    loadTasks();
+// ------JavaScript File for Page----//
 //
-    //######### TIME ################################
-    function callTime(){ // name function
-         let NowDate=(new Date()).toDateString();
-    // GET DATE
-        let NowTime=(new Date()).toLocaleTimeString(); 
-    // DOM
-    document.getElementById('time').innerHTML=`${NowDate}-${NowTime}`;
+//######### PRINT################################
+// EXPORT DOM
+    const exportor = document.getElementById('printBut');
+    exportor.addEventListener('click', exportTodoList);
+//PRINT
+    function exportTodoList() {
+      window.print();
+    }
+//
+//######### TASKS ################################
+// DOM TASKS
+    const taskInput = document.getElementById('task-input');
+    const taskList = document.getElementById('task-list');
+// ADD
+    const addTaskBtn = document.getElementById('add-task-btn');
+    addTaskBtn.addEventListener('click', addTask);
+// FUNCTION TO ADD 
+    function addTask() {
+      const taskText = taskInput.value.trim();
+      if (taskText !== '') {
+        const taskItem = document.createElement('li');
+        taskItem.classList.add('task-item');
+        const toggleCheckbox = document.createElement('input');
+        toggleCheckbox.type = 'checkbox';
+        toggleCheckbox.addEventListener('change', toggleTaskCompletion);
+        const taskTextSpan = document.createElement('span');
+        taskTextSpan.innerText = taskText;
+        const deleteBtn = document.createElement('button');
+        deleteBtn.innerText = 'x';
+        deleteBtn.addEventListener('click', deleteTask);
+        taskItem.appendChild(toggleCheckbox);
+        taskItem.appendChild(taskTextSpan);
+        taskItem.appendChild(deleteBtn);
+        taskList.appendChild(taskItem);
+        taskInput.value = '';
       }
-      // CALL FUNCTION in MS
-    setInterval(function(){  callTime() }, 1000);
+    }
+// FUNCTION TO TOGGLE
+    function toggleTaskCompletion(event) {
+      const taskElement = event.target.parentNode;
+      const taskText = taskElement.querySelector('span');
+      taskText.classList.toggle('completed');
 //
-    //################ CALENDAR ####################
-        let today = new Date();
-        let formattedDate = today.toISOString().split('T')[0];
-        document.getElementById("date").value = formattedDate;
-     
-        function saveInput() {
-          let textAreas = document.getElementsByClassName('userInput');
-          let savedInput = '';
-    
-          for (let i = 0; i < textAreas.length; i++) {
-            let userInput = textAreas[i].value;
-            savedInput += userInput + '';
-          }
-    
-          localStorage.setItem('savedInput', savedInput);
-        }
+      const toggleCheckbox = event.target;
+      if (toggleCheckbox.checked) {
+        taskElement.classList.add('completed');
+      } else {
+        taskElement.classList.remove('completed');
+      }
+    }
+// FUNCTION TO DELETE SINGLE TASK
+    function deleteTask(event) {
+      const taskItem = event.target.parentNode;
+      taskList.removeChild(taskItem);
+    }
+// DELETE ALL
+    const deleteAll = document.getElementById('deleteTodo');
+    deleteAll.addEventListener('click', deleteAllTasks);
+// FUNCTION TO DELETE ALL
+    function deleteAllTasks() {
+      taskList.innerHTML = '';
+    }
+// SAVE
+    const saveBut = document.getElementById('saveBut');
+    saveBut.addEventListener('click', saveTask);
+// FUNCTION TO SAVE
+    function saveTask() {
+      const tasks = [];
+      const taskItems = taskList.querySelectorAll('.task-item');
+      taskItems.forEach((item) => {
+        tasks.push({
+          text: item.querySelector('span').innerText,
+          completed: item.querySelector('input[type="checkbox"]').checked,
+        });
+      });
+      localStorage.setItem('savedTasks', JSON.stringify(tasks));
+      alert('Current Task List Has Been Saved. Please Select LOAD to Review Saved Tasks.');
+    }
+// LOAD
+    const loadBut = document.getElementById('load');
+    loadBut.addEventListener('click', loadTask);
+// FUNCTION TO LOAD 
+    function loadTask() {
+      const savedTasks = localStorage.getItem('savedTasks');
+      if (savedTasks) {
+        const tasks = JSON.parse(savedTasks);
+        taskList.innerHTML = '';
 //
-  //################ NOTE INPUT ####################  
-        function loadInput() {
-          var savedInput = localStorage.getItem('savedInput');
-          if (savedInput) {
-            let textAreas = document.getElementsByClassName('userInput');
-            let savedLines = savedInput.split('\n');
-            for (let i = 0; i < textAreas.length; i++) {
-              textAreas[i].value = savedLines[i];
-            }
+        tasks.forEach((task) => {
+          const taskItem = document.createElement('li');
+          taskItem.classList.add('task-item');
+          const toggleCheckbox = document.createElement('input');
+          toggleCheckbox.type = 'checkbox';
+          toggleCheckbox.addEventListener('change', toggleTaskCompletion);
+          const taskTextSpan = document.createElement('span');
+          taskTextSpan.innerText = task.text;
+          const deleteBtn = document.createElement('button');
+          deleteBtn.innerText = 'x';
+          deleteBtn.addEventListener('click', deleteTask);
+          if (task.completed) {
+            taskTextSpan.classList.add('completed');
+            toggleCheckbox.checked = true;
           }
-        }
-     // FUNCTION TO CLEAR NOTES
-        function clearInput() {
-            let textAreas = document.getElementsByClassName('userInput');
-          for (let i = 0; i < textAreas.length; i++) {
-            textAreas[i].value = '';
-          }
-          localStorage.removeItem('savedInput');
-        }
+//
+          taskItem.appendChild(toggleCheckbox);
+          taskItem.appendChild(taskTextSpan);
+          taskItem.appendChild(deleteBtn);
+          taskList.appendChild(taskItem);
+        });
+        alert('Previously Saved Tasks Have Been Successfully Loaded.');
+      } else {
+        alert('No Previously Saved Tasks Located. Please Review Workflow and Try Again.');
+      }
+    }
+//
+//######### TIME ################################
+    function callTime() { // name function
+      let NowDate = (new Date()).toDateString();
+// GET DATE
+      let NowTime = (new Date()).toLocaleTimeString();
+// DOM
+      document.getElementById('time').innerHTML = `${NowDate}-${NowTime}`;
+    }
+// CALL FUNCTION in MS
+    setInterval(function () { callTime() }, 1000);
+//
+//################ CALENDAR ####################
+    let today = new Date();
+    let formattedDate = today.toISOString().split('T')[0];
+    document.getElementById("date").value = formattedDate;
 
-        // FUNCTION TO SAVE NOTES
-        function saveInput() {
+    function saveInput() {
+      let textAreas = document.getElementsByClassName('userInput');
+      let savedInput = '';
+
+      for (let i = 0; i < textAreas.length; i++) {
+        let userInput = textAreas[i].value;
+        savedInput += userInput + '';
+      }
+
+      localStorage.setItem('savedInput', savedInput);
+    }
+//
+//################ NOTE INPUT ####################  
+    function loadInput() {
+      var savedInput = localStorage.getItem('savedInput');
+      if (savedInput) {
         let textAreas = document.getElementsByClassName('userInput');
-        let savedInput = '';
-      
+        let savedLines = savedInput.split('\n');
         for (let i = 0; i < textAreas.length; i++) {
-          let userInput = textAreas[i].value;
-          savedInput += userInput + '\n';
+          textAreas[i].value = savedLines[i];
         }
-        localStorage.setItem('savedInput', savedInput);
-        }
-        // CALL FUNCTION
-        loadInput();
-        // SAVE NOTES
-        saveButton.addEventListener('click', saveInput);
+      }
+    }
+// DELETE NOTES
+    const clearNote = document.getElementById('clear-notes');
+    clearNote.addEventListener('click', clearInput);
+// FUNCTION TO CLEAR NOTES
+    function clearInput() {
+      let textAreas = document.getElementsByClassName('userInput');
+      for (let i = 0; i < textAreas.length; i++) {
+        textAreas[i].value = '';
+      }
+      localStorage.removeItem('savedInput');
+    }
+// SAVE NOTES
+    const saveIn = document.getElementById('save-input');
+    saveIn.addEventListener('click', saveInput);
+// FUNCTION TO SAVE NOTES
+    function saveInput() {
+      let textAreas = document.getElementsByClassName('userInput');
+      let savedInput = '';
+
+      for (let i = 0; i < textAreas.length; i++) {
+        let userInput = textAreas[i].value;
+        savedInput += userInput + '\n';
+      }
+      localStorage.setItem('savedInput', savedInput);
+      alert('Current Notes Input Has Been Saved');
+    }
+// CALL FUNCTION
+    loadInput();
+// LOADING 
+    const loadNotes = document.getElementById('onLoad');
+    loadNotes.addEventListener('load', loadInput);
 //
-//
-// END OF SCRIPT // 
+//--END OF SCRIPT--//         
